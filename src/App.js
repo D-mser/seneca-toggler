ffd3e1066e59053639a50d4a290976faa30ff101
D-mser <serbu.diana.maria@gmail.com>
2021-07-09 18:08:50 +0300

    rendered question holder when data is available

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import Question from "./components/Question";
+import Button from "./components/Button";
+
+import { DataContext } from "./components/DataContext";
+import useLuckyPick from "./custom_hooks/useLuckyPick";
 
 export default function App() {
+  const { data, isFetching } = useContext(DataContext);
+  const [question, setNextQuestion] = useLuckyPick({ items: data });
+
   return (
     <>
-      <h1>Hello from the other side</h1>
+      {isFetching ? (
+        <h1>Loading...</h1>
+      ) : (
+        <>
+          <Question question={question} />
+          <Button text="Next question" onClick={setNextQuestion} />
+        </>
+      )}
     </>
   );
 }
