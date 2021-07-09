a581216ed1404840b908e01e19eb8095df12308a
D-mser <serbu.diana.maria@gmail.com>
2021-07-09 18:07:32 +0300

    wrapped App in data context provider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { DataContextProvider } from "./components/DataContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DataContextProvider url="https://raw.githubusercontent.com/D-mser/seneca-toggler/main/src/data/questions.json">
+      <App />
+    </DataContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
