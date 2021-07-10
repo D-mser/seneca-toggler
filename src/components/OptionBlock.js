009086ea4979dda35c73e6151c79197014afbbd6
D-mser <serbu.diana.maria@gmail.com>
2021-07-10 21:58:55 +0300

    optionblock returns radio buttons comp

--- src/components/OptionBlock.js
+++ src/components/OptionBlock.js
@@ -1,19 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
+import RadioButton from "./RadioButton";
+import { spaceToDash, random } from "../utils";
 
 export default function OptionBlock({ options }) {
-  const checkboxes = options.map((o, index) => (
-    <label key={index}>
-      <input type="checkbox" />
-      {o.option}
-    </label>
+  const blocks = options.map((o) => (
+    <RadioButton
+      key={spaceToDash(o.option)}
+      name={o.id}
+      label={o.option}
+      checked={true}
+    />
   ));
-  return <div>{checkboxes}</div>;
+
+  return <div className="option-block switch-toggle">{blocks}</div>;
 }
 
 OptionBlock.propTypes = {
   options: PropTypes.arrayOf(
     PropTypes.shape({
+      id: PropTypes.string,
       option: PropTypes.string,
       isSolution: PropTypes.bool,
     })
