ed182da48f05af72e04aab6568e808b08430b7ce
D-mser <serbu.diana.maria@gmail.com>
2021-07-13 10:23:54 +0300

    added conditional styles

--- src/components/OptionBlock.js
+++ src/components/OptionBlock.js
@@ -2,19 +2,21 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { spaceToDash } from "../utils";
 
-export default function OptionBlock({ options, setValidAnswer, disabled }) {
+export default function OptionBlock(props) {
   const [selected, setSelected] = useState("");
 
   function onChange(event) {
     const selectedInput = event.target.value;
     setSelected(selectedInput);
 
-    if (options.filter((o) => o.option === selectedInput)[0].isSolution) {
-      setValidAnswer();
+    if (props.options.filter((o) => o.option === selectedInput)[0].isSolution) {
+      props.setValidAnswer(props.blockId);
+    } else {
+      props.setInvalidAnswer(props.blockId);
     }
   }
 
-  const blocks = options.map((o) => (
+  const blocks = props.options.map((o) => (
     <input
       type="radio"
       key={spaceToDash(o.option)}
@@ -23,11 +25,18 @@ export default function OptionBlock({ options, setValidAnswer, disabled }) {
       onChange={onChange}
       value={o.option}
       checked={selected === o.option}
-      disabled={disabled}
+      disabled={props.disabled}
+      className={
+        selected === o.option ? props.styles.backgroundClass : "default"
+      }
     />
   ));
 
-  return <div className="option-block switch-toggle">{blocks}</div>;
+  return (
+    <div className={`option-block switch-toggle ${props.styles.borderClass}`}>
+      {blocks}
+    </div>
+  );
 }
 
 OptionBlock.propTypes = {
