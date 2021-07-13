58361dd0eecefe22484050f427e14760564e0450
D-mser <serbu.diana.maria@gmail.com>
2021-07-13 10:25:46 +0300

    changed the way valid answers are updated

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,34 +1,72 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import OptionBlock from "./OptionBlock";
+import Button from "./Button";
+import { countTrueValues } from "../utils";
+import useTheme from "../custom_hooks/useTheme";
 
-export default function Question({ question }) {
-  const [validAnswers, setValidAnswers] = useState(0);
+export default function Question({ question, setNextQuestion }) {
+  const [validAnswers, setValidAnswers] = useState({});
+
   const optionBlocks = question.answer_options_block;
+  const expectedSolutionCount = optionBlocks.length;
+  const foundSolutions = countTrueValues(validAnswers);
 
-  function validateOptionBlock() {
-    setValidAnswers((prev) => prev + 1);
+  const theme = useTheme({
+    found: foundSolutions,
+    expected: expectedSolutionCount,
+  });
+
+  function validateOptionBlock(index) {
+    setValidAnswers((prev) => {
+      return { ...prev, [index]: true };
+    });
+  }
+
+  function invalidateOptionBlock(index) {
+    setValidAnswers((prev) => {
+      return { ...prev, [index]: false };
+    });
   }
 
   const options = optionBlocks.map((optionBlock, index) => (
     <OptionBlock
-      key={index}
+      key={"" + index + expectedSolutionCount}
+      blockId={"" + index + expectedSolutionCount}
       options={optionBlock}
       setValidAnswer={validateOptionBlock}
-      disabled={optionBlocks.length === validAnswers}
+      setInvalidAnswer={invalidateOptionBlock}
+      disabled={expectedSolutionCount === foundSolutions}
+      styles={{
+        backgroundClass: theme.inputCheckedClass,
+        borderClass: theme.borderClass,
+      }}
     />
   ));
 
   useEffect(() => {
-    setValidAnswers(0);
+    setValidAnswers({});
   }, [question]);
 
   return (
-    <div>
-      <div className="h-centered">
+    <div style={{ background: theme.background }} className="question-box">
+      <div className="question-statement">
         <h1>{question.statement}</h1>
       </div>
-      <div>{options}</div>
+      <div className="all-options">{options}</div>
+      <div className="question-outcome">
+        <h2>
+          The answer is{" "}
+          {expectedSolutionCount === foundSolutions ? "correct" : "incorrect"}
+        </h2>
+      </div>
+      <div className="next-question">
+        <Button
+          text="Next question"
+          onClick={setNextQuestion}
+          className="simple-button"
+        />
+      </div>
     </div>
   );
 }
