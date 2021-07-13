0abb2561b2fabac01b37dc17f92f708cf536cf23
D-mser <serbu.diana.maria@gmail.com>
2021-07-13 10:23:07 +0300

    added function that takes in a dictionary and counts elements with true as values

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,15 @@ export function spaceToDash(str) {
 export function random(size) {
   return Math.floor(Math.random() * size);
 }
+
+export function countTrueValues(dict) {
+  let count = 0;
+
+  for (let i in dict) {
+    if (dict[i] === true) {
+      count++;
+    }
+  }
+
+  return count;
+}
